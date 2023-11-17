7279a343f77fc30e2136f60f36f2c62f431e647d
guiogigo <guilhermevbatista20@gmail.com>
2023-11-17 00:01:51 -0300

    Iniciando Profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SignIn } from './pages/SignIn';//Importa a página Details do pages
+import { SignUp } from './pages/SignUp';//Importa a página Details do pages
 import { ThemeProvider } from 'styled-components';//Prover um theme para colocar tudo no tema
 import theme from './styles/theme';//Importa o tema
 import GlobalStyles from './styles/global';
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <SignIn /> 
+      <SignUp /> 
     </ThemeProvider>
   </React.StrictMode>,
 )//Coloca o Details dentro da página HTML
